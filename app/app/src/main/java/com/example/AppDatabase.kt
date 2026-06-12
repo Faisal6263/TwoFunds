e2aa3492fcc6f11960623db22c9e30b31d75788b
Faisal6263 <68532898+Faisal6263@users.noreply.github.com>
2026-06-12 18:07:32 +0530

    Delete app/app directory

--- app/app/src/main/java/com/example/AppDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.example
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [Expense::class, DeletedTransaction::class], version = 4, exportSchema = false)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun expenseDao(): ExpenseDao
-}
